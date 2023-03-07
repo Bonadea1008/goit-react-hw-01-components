94d70055e8673cfb25c3bf6e215ab9419a3de109
Anna <100880lar@gmail.com>
2023-03-07 14:31:27 +0200

    Update styles for App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        margin: 30,
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
